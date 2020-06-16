41f07290606477e5d2bdfe2af5e9869d2f92ac7a
marlon.pedroso <marlon.pedroso@senior.com.br>
2020-06-16 18:14:08 -0300

    error-page

--- desafio-frontend/src/app/error-page/error-page.component.ts
+++ desafio-frontend/src/app/error-page/error-page.component.ts
@@ -2,6 +2,7 @@ import { Component } from "@angular/core";
 
 @Component({
   selector: "app-error-page",
-  templateUrl: "./error-page.component.html"
+  templateUrl: "./error-page.component.html",
+  styleUrls: ["./error-page.component.scss"]
 })
 export class ErrorPageComponent {}
